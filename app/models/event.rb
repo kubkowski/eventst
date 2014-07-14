d6801bce172d6f962b630c75839f241a841bf5ca
kubkowski <jakub.opara@gmail.com>
2014-07-14 13:05:46 +0200

    Fixed slugs errors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,13 @@
 class Event < ActiveRecord::Base
 	extend FriendlyId
-	friendly_id :title, use: :slugged
+	friendly_id :slug_candidates, use: :slugged
 	belongs_to :organizers, class_name: "User" 
+
+	def slug_candidates
+	[
+		:title,
+		[:title, :location],
+	]
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713192916) do
+ActiveRecord::Schema.define(version: 20140714105211) do
 
   create_table "events", force: true do |t|
     t.string   "title"
